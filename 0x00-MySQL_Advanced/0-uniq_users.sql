d9ee845ab86187639d99cffa8c8c8b195fa2a9df
cclutta <chelsea.lutta@gmail.com>
2023-01-11 19:05:22 +0300

    Update

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- creates a table called users with countries
 
-CREATE TABLE IF NOT EXISTS `users` (
-        `id` INT NOT NULL AUTO_INCREMENT,
-        `email` VARCHAR(255) NOT NULL UNIQUE,
-        `name` VARCHAR(255),
+CREATE TABLE IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
         PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- creates a table called users
 
-CREATE TABLE IF NOT EXISTS `users` (
-        `id` INT NOT NULL AUTO_INCREMENT,
-        `email` VARCHAR(255) NOT NULL UNIQUE,
-        `name` VARCHAR(255),
-        `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT `US`,
-        PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
