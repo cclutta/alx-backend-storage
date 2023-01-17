b96717088a935f76064ae71951dce3737cc93b51
cclutta <52520502+cclutta@users.noreply.github.com>
2023-01-17 21:05:58 +0300

    Create 8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+list all module
+Contains function list_all
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    list all collections
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
