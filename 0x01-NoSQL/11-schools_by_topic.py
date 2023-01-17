35aac5f9314728140450527010b8b1914171f192
cclutta <52520502+cclutta@users.noreply.github.com>
2023-01-17 21:11:58 +0300

    Create 11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Schools by topic module
+Contains the function schools_by_topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    update many rows
+    """
+    return mongo_collection.find(
+        {"topic": topic}
+    )
