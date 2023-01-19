e103475cefbd1ba4f4ecb81ba8a7683782da5deb
cclutta <chelsea.lutta@gmail.com>
2023-01-19 04:18:10 +0300

    Update

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Exercise module
+"""
+
+import redis
+
+
+class Cache:
+    """
+    Class cache
+    """
+    def __init__(self) -> None:
+        """
+        Init redis client
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in redis cache
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
